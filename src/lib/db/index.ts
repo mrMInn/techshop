1e79350a65418abb169beece8b526a35540ebc96
Minh <minhnguyenle95@gmail.com>
2026-08-06 23:41:45 +0700

    lần cuối 2

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -18,7 +18,7 @@ declare global {
 
 export const client = globalThis.postgresClient || postgres(connectionString, { 
   prepare: false, 
-  max: process.env.NODE_ENV === "production" ? undefined : 10
+  max: process.env.NODE_ENV === "production" ? 1 : 10
 });
 
 if (process.env.NODE_ENV !== "production") {
