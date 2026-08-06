614c5a674d9fe34ebae323fa52cb581d13f52f37
Minh <minhnguyenle95@gmail.com>
2026-08-07 00:34:04 +0700

    má nó

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -18,7 +18,10 @@ declare global {
 
 export const client = globalThis.postgresClient || postgres(connectionString, { 
   prepare: false, 
-  max: process.env.NODE_ENV === "production" ? 1 : 10
+  max: process.env.NODE_ENV === "production" ? 1 : 10,
+  idle_timeout: 20,
+  connect_timeout: 10,
+  ssl: process.env.NODE_ENV === "production" ? "require" : false,
 });
 
 if (process.env.NODE_ENV !== "production") {
